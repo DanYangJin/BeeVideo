a42cc323d7f2effc24b3bd2c24a6aa7f0af856fd
xiyeqiufeng <android1410danbin@163.com>
2016-04-18 09:59:39 +0800

    提交TableCell组

---
+++ BeeVideo/Classes/TableCell/SimpleTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  SimpleTableViewCell.swift
+//  BeeVideo
+//
+//  Created by DanBin on 16/4/18.
+//  Copyright © 2016年 skyworth. All rights reserved.
+//
+
+class SimpleTableViewCell: UITableViewCell {
+
+}
